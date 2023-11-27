7686f1edf32d85dcbf78125e6ae95e12203acbf8
qurriahSam <mugirethe1@gmail.com>
2023-11-27 13:40:02 +0300

    fix: remove nested a error

--- src/components/sidenav/index.tsx
+++ src/components/sidenav/index.tsx
@@ -5,40 +5,36 @@ import logo from '../../assets/logo.svg';
 
 export default function SideNav() {
   return (
-    <ul className='menu bg-base-200 w-16 md:w-20 h-screen flex justify-between py-4'>
-      <li>
-        <Link to='/' className='no-focus hover:bg-inherit'>
-          <img src={logo} alt='logo' />
-        </Link>
-      </li>
-      <span>
+    <>
+      <ul className='menu bg-base-200 w-16 md:w-20 h-screen flex justify-between py-4'>
         <li>
-          <Link to='/'>
-            <a className='tooltip tooltip-right' data-tip='Items'>
+          <Link to='/' className='no-focus hover:bg-inherit'>
+            <img src={logo} alt='logo' />
+          </Link>
+        </li>
+        <span>
+          <li>
+            <Link to='/' className='tooltip tooltip-right' data-tip='Items'>
               <i className='material-icons'>list</i>
-            </a>
-          </Link>
-        </li>
-        <li className='my-12'>
-          <Link to='history'>
-            <a className='tooltip tooltip-right' data-tip='History'>
+            </Link>
+          </li>
+          <li className='my-12'>
+            <Link to='history' className='tooltip tooltip-right' data-tip='History'>
               <i className='material-icons'>restart_alt</i>
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link to='statistics'>
-            <a className='tooltip tooltip-right' data-tip='Stats'>
+            </Link>
+          </li>
+          <li>
+            <Link to='statistics' className='tooltip tooltip-right' data-tip='Stats'>
               <i className='material-icons'>analytics</i>
-            </a>
-          </Link>
+            </Link>
+          </li>
+        </span>
+        <li className='bg-warning rounded-md'>
+          <a className='tooltip tooltip-right' data-tip='List'>
+            <i className='material-icons'>shopping_cart</i>
+          </a>
         </li>
-      </span>
-      <li className='bg-warning rounded-md'>
-        <a className='tooltip tooltip-right' data-tip='List'>
-          <i className='material-icons'>shopping_cart</i>
-        </a>
-      </li>
-    </ul>
+      </ul>
+    </>
   );
 }
