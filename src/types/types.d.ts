3bcaa95896cd0802d4f58e45c082b807176d38e0
qurriahSam <mugirethe1@gmail.com>
2023-11-28 18:33:37 +0300

    feat: add sample data

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -7,3 +7,25 @@ export interface Category {
   category: string;
   items: Item[];
 }
+
+export interface HistoryItem {
+  item: Item;
+  count: number;
+}
+
+export interface HistoryListItems {
+  category: string;
+  items: HistoryItem[];
+}
+
+export interface HistoryListItemsFull {
+  title: HistoryListTitle;
+  goods: HistoryListItems[];
+}
+
+export interface HistoryListTitle {
+  id: string;
+  name: string;
+  date: string;
+  status: string;
+}
