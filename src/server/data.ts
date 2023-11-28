4c1d03bf9d4f754b6d3d5a6fd5f47d725653860d
qurriahSam <mugirethe1@gmail.com>
2023-11-28 07:35:47 +0300

    placeholder data

---
+++ src/server/data.ts
@@ -0,0 +1,59 @@
+import { Category } from '../types/types';
+
+export const listItems: Category[] = [
+  {
+    category: 'fruit and vegetables',
+    items: [
+      { id: '1', name: 'Avocados' },
+      { id: '2', name: 'Bananas' },
+      { id: '3', name: 'Cabbage' },
+      { id: '4', name: 'skuma' },
+      { id: '5', name: 'grapes' },
+      { id: '1', name: 'Avocados' },
+      { id: '3', name: 'Cabbage' },
+      { id: '4', name: 'skuma' },
+      { id: '5', name: 'grapes' },
+      { id: '2', name: 'Bananas' },
+    ],
+  },
+  {
+    category: 'Beverages',
+    items: [
+      { id: '6', name: 'Soda' },
+      { id: '7', name: 'Beer' },
+      { id: '8', name: 'Water' },
+      { id: '9', name: 'Wine' },
+      { id: '10', name: 'Juice' },
+    ],
+  },
+  {
+    category: 'Meat & Fish',
+    items: [
+      { id: '1', name: 'Avocados' },
+      { id: '2', name: 'Bananas' },
+      { id: '3', name: 'Cabbage' },
+      { id: '4', name: 'skuma' },
+      { id: '5', name: 'grapes' },
+      { id: '1', name: 'Avocados' },
+      { id: '3', name: 'Cabbage' },
+      { id: '4', name: 'skuma' },
+      { id: '5', name: 'grapes' },
+      { id: '2', name: 'Bananas' },
+    ],
+  },
+  {
+    category: 'pets',
+    items: [
+      { id: '1', name: 'Avocados' },
+      { id: '2', name: 'Bananas' },
+      { id: '3', name: 'Cabbage' },
+      { id: '4', name: 'skuma' },
+      { id: '5', name: 'grapes' },
+      { id: '1', name: 'Avocados' },
+      { id: '3', name: 'Cabbage' },
+      { id: '4', name: 'skuma' },
+      { id: '5', name: 'grapes' },
+      { id: '2', name: 'Bananas' },
+    ],
+  },
+];
