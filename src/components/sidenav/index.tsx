5559de49004371c7a594aacfbfb455cd9bdcbd5a
qurriahSam <mugirethe1@gmail.com>
2023-11-29 21:59:15 +0300

    feat: add history route

--- src/components/sidenav/index.tsx
+++ src/components/sidenav/index.tsx
@@ -5,36 +5,34 @@ import logo from '../../assets/logo.svg';
 
 export default function SideNav() {
   return (
-    <>
-      <ul className='menu w-16 md:w-20 h-screen flex justify-between py-4'>
+    <div className='w-16 md:w-20 h-screen flex flex-col justify-between py-4'>
+      <Link to='/' className='ps-2 md:ps-4'>
+        <img src={logo} alt='logo' />
+      </Link>
+      <ul className='menu py-4'>
         <li>
-          <Link to='/' className='no-focus hover:bg-inherit'>
-            <img src={logo} alt='logo' />
+          <Link to='/' className='tooltip tooltip-right' data-tip='Items'>
+            <i className='material-icons'>list</i>
           </Link>
         </li>
-        <span>
-          <li>
-            <Link to='/' className='tooltip tooltip-right' data-tip='Items'>
-              <i className='material-icons'>list</i>
-            </Link>
-          </li>
-          <li className='my-12'>
-            <Link to='history' className='tooltip tooltip-right' data-tip='History'>
-              <i className='material-icons'>restart_alt</i>
-            </Link>
-          </li>
-          <li>
-            <Link to='statistics' className='tooltip tooltip-right' data-tip='Stats'>
-              <i className='material-icons'>analytics</i>
-            </Link>
-          </li>
-        </span>
-        <li className='bg-warning rounded-md'>
-          <a className='tooltip tooltip-right' data-tip='List'>
-            <i className='material-icons'>shopping_cart</i>
-          </a>
+        <li className='my-12'>
+          <Link to='history' className='tooltip tooltip-right' data-tip='History'>
+            <i className='material-icons'>restart_alt</i>
+          </Link>
+        </li>
+        <li>
+          <Link to='statistics' className='tooltip tooltip-right' data-tip='Stats'>
+            <i className='material-icons'>analytics</i>
+          </Link>
         </li>
       </ul>
-    </>
+      <div className='ps-2 md:ps-4'>
+        <button className='btn btn-primary btn-circle shadow'>
+          <a className='tooltip tooltip-right font-normal' data-tip='List'>
+            <i className='material-icons'>shopping_cart</i>
+          </a>
+        </button>
+      </div>
+    </div>
   );
 }
