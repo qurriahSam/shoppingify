b66cf7c4a21291a080c292bc3df6684d2d7be5b2
qurriahSam <mugirethe1@gmail.com>
2023-11-29 21:12:32 +0300

    feat: customize theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /*eslint-env node*/
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -6,17 +7,16 @@ export default {
     extend: {},
   },
 
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
   plugins: [require('daisyui'), require('tailwind-scrollbar')({ nocompatible: true })],
 
   daisyui: {
-    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: 'dark', // name of one of the included themes for dark mode
-    base: true, // applies background color and foreground color for root element by default
-    styled: true, // include daisyUI colors and design decisions for all components
-    utils: true, // adds responsive and modifier utility classes
-    prefix: '', // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-    themeRoot: ':root', // The element that receives theme color CSS variables
+    themes: [
+      {
+        light: {
+          ...require('daisyui/src/theming/themes')['light'],
+          primary: '#F9A109',
+        },
+      },
+    ],
   },
 };
