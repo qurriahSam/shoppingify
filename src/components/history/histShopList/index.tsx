021b662fb839d7707cf7cf5371755805cc4a86d6
qurriahSam <mugirethe1@gmail.com>
2023-11-30 20:04:38 +0300

    feat: display history items

--- src/components/history/histShopList/index.tsx
+++ src/components/history/histShopList/index.tsx
@@ -1,24 +1,43 @@
-function DisplayHistItem() {
+import { historyListItems } from '../../../server/data';
+import { HistoryItem, HistoryListItems /* HistoryListItemsFull */ } from '../../../types/types';
+
+function DisplayHistItem({ item }: { item: HistoryItem }) {
   return (
-    <div className='text-start font-normal flex flex-wrap p-2 m-2 rounded-lg shadow-lg text-sm bg-base-100 max-w-xs'>
-      <p className='me-1 p-1'>cookies</p>
-      <p className='me-1 p-1'>3pcs</p>
+    <div className='text-start font-normal flex flex-wrap p-2 m-2 rounded-lg shadow-lg text-sm bg-base-100 w-fit'>
+      <p className='me-1 p-1'>{item.item.name}</p>
+      <p className='me-1 p-1 text-primary'>{item.count}</p>
     </div>
   );
 }
-export default function HistShopList() {
+
+function DisplayCategory({ histList }: { histList: HistoryListItems }) {
+  return (
+    <div>
+      <p className='font-medium'>{histList.category}</p>
+      <div className='flex flex-wrap'>
+        {histList.items.map((item, index) => (
+          <DisplayHistItem key={index} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default function HistShopList(/* { histListTitle }: { histListTitle: HistoryListItemsFull } */) {
   return (
     <div>
       <button className='btn btn-link text-sm no-underline hover:no-underline'>
         <i className='material-icons text-sm font-semibold'>arrow_back</i>Back
       </button>
-      <h2 className='mb-2 font-medium'>Eeros farewel party</h2>
+      <h2 className='mb-2 font-medium'>{historyListItems.title.name}</h2>
       <span className='flex text-neutral-500'>
         <i className='material-icons text-xs mr-1'>event_note</i>
-        <p className='text-xs font-medium'>10 march 2023</p>
+        <p className='text-xs font-medium'>{historyListItems.title.date}</p>
       </span>
       <div>
-        <DisplayHistItem />
+        {historyListItems.goods.map((good, index) => (
+          <DisplayCategory key={index} histList={good} />
+        ))}
       </div>
     </div>
   );
