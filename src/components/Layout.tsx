54b9f860196c127d96d25bfc81dc5da129d3b55c
qurriahSam <mugirethe1@gmail.com>
2023-11-30 20:02:52 +0300

    feat: display drawer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 import SideNav from './sidenav';
+import Drawer from './shopping/drawer';
 
 export default function SiteWrapper({ children }: { children: ReactNode }) {
   return (
@@ -7,7 +8,12 @@ export default function SiteWrapper({ children }: { children: ReactNode }) {
       <div>
         <SideNav />
       </div>
-      <div className='px-6 md:px-12 py-8 w-full overflow-y-auto bg-base-200'>{children}</div>
+      <div className='px-6 md:px-12 py-8 w-full md:w-2/3 lg:w-3/4 overflow-y-auto bg-base-200'>
+        {children}
+      </div>
+      <div className='hidden md:w-1/3 lg:w-1/4 md:block'>
+        <Drawer />
+      </div>
     </div>
   );
 }
