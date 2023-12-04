e3356657841085ea814452624e307d881c8a0170
qurriahSam <mugirethe1@gmail.com>
2023-12-04 20:24:46 +0300

    feat: enable toggle of edit mode

--- src/components/shopping/list/index.tsx
+++ src/components/shopping/list/index.tsx
@@ -1,8 +1,9 @@
+import { useState } from 'react';
 import wine from '../../../assets/source.svg';
 import CheckList from './checkList';
 import CreateList from './createList';
 
-function ListSave() {
+function ListSave({ toggleEditMode }: { toggleEditMode: () => void }) {
   return (
     <form className='flex mt-2 h-fit w-fit rounded-lg shadow border bg-base-100 focus-within:border-primary'>
       <input
@@ -10,38 +11,48 @@ function ListSave() {
         placeholder='Enter a name'
         className='input input-xs max-w-sm focus:border-0 outlineR focus-within:border-0'
       />
-      <button className='btn btn-primary btn-sm'>Save</button>
+      <button className='btn btn-primary btn-sm' onClick={toggleEditMode}>
+        Save
+      </button>
     </form>
   );
 }
 
-function CancelComplete() {
+function CancelComplete({ toggleEditMode }: { toggleEditMode: () => void }) {
   return (
     <div className='flex justify-center'>
-      <button className='btn btn-link no-underline hover:no-underline'>cancel</button>
+      <button className='btn btn-link no-underline hover:no-underline' onClick={toggleEditMode}>
+        cancel
+      </button>
       <button className='btn btn-primary'>Save</button>
     </div>
   );
 }
 
-export default function List() {
+export default function List({ toggleNewItem }: { toggleNewItem: () => void }) {
+  const [editMode, setEditMode] = useState(true);
+
+  const toggleEditMode = () => setEditMode(!editMode);
+
   return (
     <div className='p-5 h-full flex flex-col'>
       <div>
         <div className='bg-secondary rounded-[2.5rem] relative w-[16rem] h-32'>
           <div className='absolute right-1 top-4 w-[10rem] '>
             <p className='mb-3 font-semibold text-neutral-300'>Did you find what you need?</p>
-            <button className='btn btn-primary btn-sm px-5'>Add Item</button>
+            <button className='btn btn-primary btn-sm px-5' onClick={toggleNewItem}>
+              Add Item
+            </button>
           </div>
         </div>
         <img src={wine} alt='' className='absolute top-0.5' />
       </div>
-      <div className='grow'>
-        {/*  <CreateList /> */}
-        <CheckList />
-      </div>
-      {/* <ListSave /> */}
-      <CancelComplete />
+      <div className='grow'>{editMode ? <CreateList /> : <CheckList />}</div>
+      {editMode ? (
+        <ListSave toggleEditMode={toggleEditMode} />
+      ) : (
+        <CancelComplete toggleEditMode={toggleEditMode} />
+      )}
     </div>
   );
 }
