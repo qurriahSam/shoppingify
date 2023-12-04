e657d79965caccf88f4282615c599898f30012e9
qurriahSam <mugirethe1@gmail.com>
2023-12-04 20:23:50 +0300

    feat: toggle new item

--- src/components/shopping/drawer.tsx
+++ src/components/shopping/drawer.tsx
@@ -1,6 +1,16 @@
+import { useState } from 'react';
 import './drawer.css';
 import List from './list';
+import AddNewItem from './addNewItem';
 
 export default function Drawer() {
-  return <List />;
+  const [newItem, setNewItemDisp] = useState(false);
+
+  const toggleNewItem = () => {
+    console.log(newItem);
+
+    setNewItemDisp(!newItem);
+  };
+
+  return <>{newItem ? <AddNewItem /> : <List toggleNewItem={toggleNewItem} />}</>;
 }
