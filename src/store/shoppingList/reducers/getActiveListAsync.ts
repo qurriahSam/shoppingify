d08e5a1a748bd0ebfd385254be91d0b96ec52389
qurriahSam <mugirethe1@gmail.com>
2024-02-25 14:28:17 +0300

    test

--- src/store/shoppingList/reducers/getActiveListAsync.ts
+++ src/store/shoppingList/reducers/getActiveListAsync.ts
@@ -7,6 +7,7 @@ const getActiveShoppingList = createAsyncThunk(
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   async () => {
     const URL = process.env.REACT_APP_API_URL;
+    console.log(URL);
     try {
       const response = await axios(`${URL}`);
       if (response.data === null) {
