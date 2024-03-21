2e2f9564e4da73810e7653a92bc5e55c483e31ce
qurriahSam <mugirethe1@gmail.com>
2024-03-21 22:55:37 +0300

    fix: prevent overload error

--- src/store/category/reducers/fetchItemsAsyncReducer.ts
+++ src/store/category/reducers/fetchItemsAsyncReducer.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 const fetchCategories = createAsyncThunk('category/fetchCategories', async () => {
   //const URL = process.env.REACT_APP_API_URL;
   try {
-    const response = await axios.get('https://shoppingify-h8cg.onrender.com');
+    const response = await axios.get('http://localhost:3000');
     return response.data;
   } catch (error) {
     console.error(error);
