5716dee61c1651286e4279bafa709964e60c55dd
qurriahSam <mugirethe1@gmail.com>
2024-03-27 15:51:09 +0300

    feat: add indicator to display number of items in list

--- src/components/ui/SideNav/SideNav.tsx
+++ src/components/ui/SideNav/SideNav.tsx
@@ -3,10 +3,23 @@ import { Link } from 'react-router-dom';
 
 import logo from '../../../assets/logo.svg';
 import { ThemeToggle } from './ThemeToggle';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../store/store';
+import { Status } from '../../../types/types';
 
 type ToggleDrawer = () => void;
 
 export default function SideNav({ toggleDrawer }: { toggleDrawer: ToggleDrawer }) {
+  const shopList = useSelector((state: RootState) => state.shoppingList);
+
+  function getLen() {
+    let len = 0;
+    shopList.list.forEach((cat) => {
+      len += cat.items.length;
+    });
+    return len;
+  }
+
   return (
     <div className='w-16 md:w-20 h-screen flex flex-col justify-between py-4'>
       <Link to='/app' className='ps-2 md:ps-4'>
@@ -32,7 +45,13 @@ export default function SideNav({ toggleDrawer }: { toggleDrawer: ToggleDrawer }
       <ThemeToggle />
       <div className='ps-2 md:ps-4'>
         <button className='btn btn-primary btn-circle shadow' onClick={toggleDrawer}>
-          <a className='tooltip tooltip-right font-normal' data-tip='List'>
+          <a className='tooltip tooltip-right font-normal indicator' data-tip='List'>
+            {shopList.update === Status.updated && shopList.list.length > 0 ? (
+              <span className='indicator-item badge badge-secondary text-xs font-medium'>
+                {getLen()}
+              </span>
+            ) : null}
+
             <i className='material-symbols-outlined'>shopping_cart</i>
           </a>
         </button>
