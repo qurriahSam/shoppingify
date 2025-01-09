f81f8c9e33685cd29ee4892a05e35b6971ccc51d
qurriahSam <mugirethe1@gmail.com>
2025-01-09 09:05:20 +0300

    feat: create login component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
-import SiteWrapper from './components/Layout';
-import Home from './pages/Home/Home';
-import History from './pages/History/History';
-import Stats from './pages/Stats/Stats';
-import HistShopList from './components/ui/History/HistShopList';
-import PageNotFound from './pages/pageNotFound/PageNotFound';
-import LandingPage from './pages/LandingPage/LandingPage';
+import SiteWrapper from "./components/Layout";
+import Home from "./pages/Home/Home";
+import History from "./pages/History/History";
+import Stats from "./pages/Stats/Stats";
+import HistShopList from "./components/ui/History/HistShopList";
+import PageNotFound from "./pages/pageNotFound/PageNotFound";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <Routes>
-      <Route path='/app' Component={SiteWrapper}>
+      <Route path="/app" Component={SiteWrapper}>
         <Route index Component={Home} />
-        <Route path='history' Component={History} />
-        <Route path='history/:id' Component={HistShopList} />
-        <Route path='statistics' Component={Stats} />
+        <Route path="history" Component={History} />
+        <Route path="history/:id" Component={HistShopList} />
+        <Route path="statistics" Component={Stats} />
       </Route>
-      <Route path='/' Component={LandingPage} />
-      <Route path='*' Component={PageNotFound} />
+      <Route path="login" Component={Login} />
+      <Route path="/" Component={LandingPage} />
+      <Route path="*" Component={PageNotFound} />
     </Routes>
   );
 }
