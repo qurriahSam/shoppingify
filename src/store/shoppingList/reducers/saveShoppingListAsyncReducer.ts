1d7d58155df94c7a68f510119aeebe40a65a55c7
masichaivy <lyndamasicha@gmail.com>
2025-01-16 19:10:15 +0300

    feat: add user id to new shopping list

--- src/store/shoppingList/reducers/saveShoppingListAsyncReducer.ts
+++ src/store/shoppingList/reducers/saveShoppingListAsyncReducer.ts
@@ -1,13 +1,16 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import { ShoppingList, Status } from '../../../types/types';
-import axios from 'axios';
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { ShoppingList, Status } from "../../../types/types";
+import axios from "axios";
+import useLocalStorage from "../../../service/localStorageService";
 
 const saveShoppingList = createAsyncThunk(
-  'shoppingList/saveShoppingList',
+  "shoppingList/saveShoppingList",
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   async (shoppingList: any): Promise<ShoppingList> => {
-    //const URL = process.env.API_URL;
+    const URL = process.env.LOCAL_API_URL;
+    const { userDetails } = useLocalStorage("user");
     const sendData = {
+      userId: userDetails?._id,
       title: shoppingList.title,
       list: shoppingList.list,
       status: shoppingList.status,
@@ -18,15 +21,12 @@ const saveShoppingList = createAsyncThunk(
       if (shoppingList._id.length > 1) {
         console.log(shoppingList);
         const response = await axios.post(
-          `https://shoppingify-h8cg.onrender.com/updateShopping`,
+          `${URL}/updateShopping`,
           shoppingList
         );
         return { ...response.data, update: Status.updated };
       }
-      const response = await axios.post(
-        `https://shoppingify-h8cg.onrender.com/newShopping`,
-        sendData
-      );
+      const response = await axios.post(`${URL}/newShopping`, sendData);
       return { ...response.data, update: Status.updated };
     } catch (error) {
       console.log(error);
