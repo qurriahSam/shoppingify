b25147b1f6605df57828c299e2e14c81320d43e7
masichaivy <lyndamasicha@gmail.com>
2025-01-21 16:07:37 +0300

    fix: get backend url from env file

--- src/store/auth/reducers/registerUserAsyncReducer.ts
+++ src/store/auth/reducers/registerUserAsyncReducer.ts
@@ -1,5 +1,5 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 interface INewUser {
   email: string;
@@ -7,12 +7,12 @@ interface INewUser {
 }
 
 const registerUser = createAsyncThunk(
-  "auth/registerUser",
+  'auth/registerUser',
   async (user: INewUser) => {
-    //const URL = process.env.REACT_APP_API_URL;
+    const URL = process.env.API_URL;
 
     try {
-      const response = await axios.post("http://localhost:3000/register", user);
+      const response = await axios.post(`${URL}/register`, user);
       console.log(response);
       return response.data;
     } catch (error) {
