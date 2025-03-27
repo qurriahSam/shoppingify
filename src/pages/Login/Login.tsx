698b5718084d3e4723984540358fceb3cb669104
qurriahSam <mugirethe1@gmail.com>
2025-03-27 13:51:30 +0300

    feat: add demo user login functionality

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -54,6 +54,10 @@ export default function Login() {
     validateField(key, value);
   };
 
+  const addDemoUserDetails = () => {
+    setUserDetails({ email: 'user.demo@demo.com', password: '123456' });
+  };
+
   const isFormValid = !errors.email && !errors.password;
 
   const loginNewUser = (e: SyntheticEvent) => {
@@ -62,40 +66,40 @@ export default function Login() {
   };
 
   return (
-    <section className='flex justify-center items-center h-screen'>
-      <div className='card w-96 bg-base-100 shadow-xl'>
-        <div className='card-body'>
-          <h2 className='card-title'>Shoppingify</h2>
+    <section className="flex justify-center items-center h-screen">
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Shoppingify</h2>
           {false && (
-            <div className='label' hidden>
-              <span className='label-text-alt text-error'>
+            <div className="label" hidden>
+              <span className="label-text-alt text-error">
                 Wrong Email or Password.
               </span>
             </div>
           )}
           <form onSubmit={loginNewUser}>
-            <label className='input input-bordered flex items-center gap-2'>
+            <label className="input input-bordered flex items-center gap-2">
               <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 16 16'
-                fill='currentColor'
-                className='w-4 h-4 opacity-70'
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="w-4 h-4 opacity-70"
               >
-                <path d='M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z' />
-                <path d='M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z' />
+                <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
               </svg>
               <input
-                id='email'
-                type='text'
-                className='grow'
-                placeholder='Email'
+                id="email"
+                type="text"
+                className="grow"
+                placeholder="Email"
                 value={userDetails.email}
                 onChange={updateUserDetails}
               />
             </label>
             {errors.email ? (
-              <div className='label'>
-                <span className='label-text-alt text-error'>
+              <div className="label">
+                <span className="label-text-alt text-error">
                   {errors.email}
                 </span>
               </div>
@@ -103,31 +107,31 @@ export default function Login() {
               <br />
             )}
 
-            <label className='input input-bordered flex items-center gap-2'>
+            <label className="input input-bordered flex items-center gap-2">
               <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 16 16'
-                fill='currentColor'
-                className='w-4 h-4 opacity-70'
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="w-4 h-4 opacity-70"
               >
                 <path
-                  fillRule='evenodd'
-                  d='M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z'
-                  clipRule='evenodd'
+                  fillRule="evenodd"
+                  d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                  clipRule="evenodd"
                 />
               </svg>
               <input
-                id='password'
-                type='password'
-                className='grow'
-                placeholder='password'
+                id="password"
+                type="password"
+                className="grow"
+                placeholder="password"
                 value={userDetails.password}
                 onChange={updateUserDetails}
               />
             </label>
             {errors.password ? (
-              <div className='label'>
-                <span className='label-text-alt text-error'>
+              <div className="label">
+                <span className="label-text-alt text-error">
                   {errors.password}
                 </span>
               </div>
@@ -136,7 +140,7 @@ export default function Login() {
             )}
 
             <button
-              className='btn btn-primary btn-block'
+              className="btn btn-primary btn-block"
               disabled={
                 !isFormValid ||
                 userDetails.email.length < 1 ||
@@ -145,17 +149,24 @@ export default function Login() {
               }
             >
               {auth.status === Status.loading ? (
-                <span className='loading loading-dots loading-sm'></span>
+                <span className="loading loading-dots loading-sm"></span>
               ) : (
                 'Login'
               )}
             </button>
           </form>
-          <p className='mt-3'>
-            Don't have an account?
-            <a className='link link-primary' onClick={navToRegister}>
+          <p className="mt-3">
+            Don't have an account?{' '}
+            <a className="link link-primary" onClick={navToRegister}>
               Register
             </a>
+          </p>
+          <p className="mt-3">
+            Use{' '}
+            <a className="link link-primary" onClick={addDemoUserDetails}>
+              demo
+            </a>{' '}
+            account
           </p>
         </div>
       </div>
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -101,127 +101,128 @@ export default function Register() {
   };
 
   return (
-    <section className='flex justify-center items-center h-screen relative'>
-      <div className='card w-96 bg-base-100 shadow-xl'>
-        <div className='card-body'>
-          <h2 className='card-title'>Shoppingify</h2>
+    <section className="flex justify-center items-center h-screen relative">
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Shoppingify</h2>
           <br />
           <form onSubmit={registerNewUser}>
-            <label className='input input-bordered flex items-center gap-2'>
+            <label className="input input-bordered flex items-center gap-2">
               <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 16 16'
-                fill='currentColor'
-                className='w-4 h-4 opacity-70'
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="w-4 h-4 opacity-70"
               >
-                <path d='M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z' />
-                <path d='M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z' />
+                <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
               </svg>
               <input
-                id='email'
-                type='text'
-                className='grow'
-                placeholder='Email'
+                id="email"
+                type="text"
+                className="grow"
+                placeholder="Email"
                 value={userDetails.email}
                 onChange={updateUserDetails}
               />
             </label>
             {errors.email && (
-              <p className='text-red-500 text-sm'>{errors.email}</p>
+              <p className="text-red-500 text-sm">{errors.email}</p>
             )}
             <br />
-            <label className='input input-bordered flex items-center gap-2'>
+            <label className="input input-bordered flex items-center gap-2">
               <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 16 16'
-                fill='currentColor'
-                className='w-4 h-4 opacity-70'
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="w-4 h-4 opacity-70"
               >
-                <path d='M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z' />
+                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
               </svg>
               <input
-                type='text'
-                className='grow'
-                placeholder='Username'
-                id='username'
+                type="text"
+                className="grow"
+                placeholder="Username"
+                id="username"
                 value={userDetails.username}
                 onChange={updateUserDetails}
               />
             </label>
             {errors.username && (
-              <p className='text-red-500 text-sm'>{errors.username}</p>
+              <p className="text-red-500 text-sm">{errors.username}</p>
             )}
             <br />
-            <label className='input input-bordered flex items-center gap-2'>
+            <label className="input input-bordered flex items-center gap-2">
               <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 16 16'
-                fill='currentColor'
-                className='w-4 h-4 opacity-70'
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="w-4 h-4 opacity-70"
               >
                 <path
-                  fillRule='evenodd'
-                  d='M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z'
-                  clipRule='evenodd'
+                  fillRule="evenodd"
+                  d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                  clipRule="evenodd"
                 />
               </svg>
               <input
-                type='password'
-                className='grow'
-                placeholder='password'
-                id='password'
+                type="password"
+                className="grow"
+                placeholder="password"
+                id="password"
                 value={userDetails.password}
                 onChange={updateUserDetails}
               />
             </label>
             {errors.password && (
-              <p className='text-red-500 text-sm'>{errors.password}</p>
+              <p className="text-red-500 text-sm">{errors.password}</p>
             )}
             <br />
-            <label className='input input-bordered flex items-center gap-2'>
+            <label className="input input-bordered flex items-center gap-2">
               <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 16 16'
-                fill='currentColor'
-                className='w-4 h-4 opacity-70'
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="w-4 h-4 opacity-70"
               >
                 <path
-                  fillRule='evenodd'
-                  d='M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z'
-                  clipRule='evenodd'
+                  fillRule="evenodd"
+                  d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                  clipRule="evenodd"
                 />
               </svg>
               <input
-                id='confirmPassword'
-                type='password'
-                className='grow'
-                placeholder='confirm password'
+                id="confirmPassword"
+                type="password"
+                className="grow"
+                placeholder="confirm password"
                 value={userDetails.confirmPassword}
                 onChange={updateUserDetails}
               />
             </label>
             {errors.confirmPassword && (
-              <p className='text-red-500 text-sm'>{errors.confirmPassword}</p>
+              <p className="text-red-500 text-sm">{errors.confirmPassword}</p>
             )}
             <br />
             <button
-              className='btn btn-primary btn-block'
+              className="btn btn-primary btn-block"
               disabled={hasErrors || auth.status === Status.loading}
             >
               {auth.status === Status.loading ? (
-                <span className='loading loading-dots loading-sm'></span>
+                <span className="loading loading-dots loading-sm"></span>
               ) : (
                 'Register'
               )}
             </button>
           </form>
 
-          <p className='mt-5'>
+          <p className="mt-5">
             Already have an account?
-            <a className='link link-primary' onClick={navToLogin}>
+            <a className="link link-primary" onClick={navToLogin}>
               Login
             </a>
           </p>
+          <p>To use demo account go to login</p>
         </div>
       </div>
     </section>
